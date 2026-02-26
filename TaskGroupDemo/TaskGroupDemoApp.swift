6655877091ccb1d52f9431561784c52a5bb9cefb
AbrahamGlzP <abrahamglzp@gmail.com>
2026-02-26 13:01:07 -0600

    Initial Commit

---
+++ TaskGroupDemo/TaskGroupDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskGroupDemoApp.swift
+//  TaskGroupDemo
+//
+//  Created by Abraham Gonzalez Puga on 26/02/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskGroupDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
